53082a01ce3b3c538977e677165386473562bab3
ertekessimon <ertekessimon@gmail.com>
2022-07-22 15:43:39 +0200

    update

--- main/src/views/AboutView.vue
+++ main/src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="server">
+    <h3>Minecraft:170-187-186-233.ip.linodeusercontent.com<h3>
+    <h3>https://discord.gg/xYa2fTz6<h3>
   </div>
 </template>
