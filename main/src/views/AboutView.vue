9e489a422b2c97222ed89af1fed6b9526604f817
ertekessimon <ertekessimon@gmail.com>
2022-07-22 15:43:51 +0200

    update

--- main/src/views/AboutView.vue
+++ main/src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="server">
-    <h3>Minecraft:170-187-186-233.ip.linodeusercontent.com<h3>
-    <h3>https://discord.gg/xYa2fTz6<h3>
+  <div class="Server">
+    <h1>Discord:https://discord.gg/xYa2fTz6</h1>
+    <h1>Minecraft:170-187-186-233.ip.linodeusercontent.com</h1>
   </div>
 </template>
