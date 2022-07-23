ce16f25ab9b2c89b4d6a577a0365d70e4f2b934b
ertekessimon <ertekessimon@gmail.com>
2022-07-23 10:39:41 +0200

    xdd

--- main/src/views/HomeView.vue
+++ main/src/views/HomeView.vue
@@ -8,7 +8,6 @@
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
 @Options({
   components: {
     HelloWorld,
